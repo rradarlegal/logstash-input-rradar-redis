e3217a69ecee2c965f1740c55646b538ba619ec0
Dave Almond <david.almond@rradar.com>
2022-07-12 15:00:32 +0100

    plugin naming correction

--- lib/logstash/inputs/redis.rb
+++ lib/logstash/inputs/redis.rb
@@ -17,10 +17,10 @@ require "stud/interval"
 # `batch_count` note: If you use the `batch_count` setting, you *must* use a Redis version 2.6.0 or
 # newer. Anything older does not support the operations used by batching.
 #
-module LogStash module Inputs class Redis < LogStash::Inputs::Threadable
+module LogStash module Inputs class Rredis < LogStash::Inputs::Threadable
   BATCH_EMPTY_SLEEP = 0.25
 
-  config_name "rradar-redis"
+  config_name "rredis"
 
   default :codec, "json"
 
